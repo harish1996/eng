2c641a755f1ec646ed46bf66c41967e615bd7411
Rishabh Chaurasia <dksch007@gmail.com>
2019-07-23 12:59:52 +0530

    Added preliminary version of refs management

---
+++ src/refs/refs.h
@@ -0,0 +1,16 @@
+#ifndef _REFS_H
+#define _REFS_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char* read_branch(char []);
+
+int write_branch(char [], char[]);
+
+char *getHEAD();
+
+int writeHEAD(char []);
+
+#endif
